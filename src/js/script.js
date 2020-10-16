a7e961155bc3590c924a4fe1ce5f5793e31d0e82
Vadim <hellovados@gmail.com>
2020-10-16 17:33:04 +0300

    we add mail and we using mamp for local server

--- src/js/script.js
+++ src/js/script.js
@@ -81,21 +81,21 @@ $(document).ready(function(){
 
     $('[name=phone').mask("+7 (999) 999-99-99");
 
-    $('form').submit(function(e){
+    $('form').submit(function(e) {
       e.preventDefault();
       $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-        }).done(function(){
-          $(this).find("value").val("");
-          $('#consultation', '#order').fadeOut();
-          $('.overlay', '#thanks').fadeIn('slow');
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
 
           $('form').trigger('reset');
-        })
+      });
+      return false;
     });
-    return false;
     
   });
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = '05031992vg';                           // Наш пароль
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->setFrom('hellovados@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('yekada2401@ptcji.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
