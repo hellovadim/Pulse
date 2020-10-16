7e802aafa101f7e0e982266cb859347c54e3bc1f
Vadim <hellovados@gmail.com>
2020-10-16 15:50:13 +0300

    we add php/mailer

--- src/js/script.js
+++ src/js/script.js
@@ -80,6 +80,22 @@ $(document).ready(function(){
     validateForm('#order form');
 
     $('[name=phone').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+        }).done(function(){
+          $(this).find("value").val("");
+          $('#consultation', '#order').fadeOut();
+          $('.overlay', '#thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+        })
+    });
+    return false;
     
   });
 
