c78969c79450174e053aebbced78103c76929708
Vadim <hellovados@gmail.com>
2020-10-17 00:52:33 +0300

    we add scroll to up

--- src/js/script.js
+++ src/js/script.js
@@ -96,6 +96,19 @@ $(document).ready(function(){
       });
       return false;
     });
-    
+    // smoth scroll pageup
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 1600){
+        $('.pageup').fadeIn();
+      } else{
+        $('.pageup').fadeOut ();
+      }
+    });
+    $("a[href^='#']").click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+});
   });
 
